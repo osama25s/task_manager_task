879f6aa5918372e4cc9032deefa2efbb0f1b14f6
Osama Saad <117564564+osama25s@users.noreply.github.com>
2025-10-02 13:07:15 +0300

    finish task

---
+++ android/app/src/main/kotlin/com/example/task_manager_task/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.task_manager_task
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const TaskManagerPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class TaskModel {
+  final String id;
+  final String title;
+  final IconData icon;
+  bool isDone;
+
+  TaskModel({
+    required this.id,
+    required this.title,
+    required this.icon,
+    this.isDone = false,
+  });
+}
+
+class TaskManagerPage extends StatefulWidget {
+  const TaskManagerPage({super.key});
+
+  @override
+  State<TaskManagerPage> createState() => _TaskManagerPageState();
+}
+
+class _TaskManagerPageState extends State<TaskManagerPage> {
+  List<TaskModel> tasks = [
+    TaskModel(
+      id: "1",
+      title: "Complete Flutter Assignment",
+      icon: Icons.drag_handle,
+    ),
+    TaskModel(
+      id: "2",
+      title: "Review Clean Architecture",
+      icon: Icons.drag_handle,
+    ),
+    TaskModel(
+      id: "3",
+      title: "Practice Widget Catalog",
+      icon: Icons.drag_handle,
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.cyan[800],
+        title: const Text(
+          'Task Manager Page',
+          style: TextStyle(
+            fontSize: 26,
+            fontWeight: FontWeight.w600,
+            color: Colors.white,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: ReorderableListView.builder(
+        itemCount: tasks.length,
+        itemBuilder: (_, index) {
+          final task = tasks[index];
+          return TaskCard(
+            key: ValueKey(task.id),
+            task: task,
+            onChanged: (val) {
+              setState(() {
+                task.isDone = val ?? false;
+              });
+            },
+          );
+        },
+        onReorder: (oldIndex, newIndex) {
+          setState(() {
+            if (oldIndex < newIndex) newIndex -= 1;
+            final item = tasks.removeAt(oldIndex);
+            tasks.insert(newIndex, item);
+          });
+        },
+      ),
+    );
+  }
+}
+
+class TaskCard extends StatelessWidget {
+  final TaskModel task;
+  final ValueChanged<bool?> onChanged;
+
+  const TaskCard({super.key, required this.task, required this.onChanged});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(12),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.grey[300],
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: CheckboxListTile(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16),
+          ),
+          value: task.isDone,
+          onChanged: onChanged,
+          dense: true,
+          contentPadding: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 10,
+          ),
+          title: Row(
+            children: [
+              Icon(task.icon, color: Colors.black54, size: 30),
+              const SizedBox(width: 15),
+              Expanded(
+                child: Text(
+                  task.title,
+                  style: TextStyle(
+                    fontSize: 14,
+                    decoration:
+                        task.isDone
+                            ? TextDecoration.lineThrough
+                            : TextDecoration.none,
+                    decorationColor: Colors.grey[600],
+                    decorationThickness: 2,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
